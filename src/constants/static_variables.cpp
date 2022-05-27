5d31fb1777b5496c83415342c6fbccc253e951ce
Charlie-Porto <charlie@d20.solutions>
2022-05-26 19:13:59 -0500

    minor pruning in static_variables

--- src/constants/static_variables.cpp
+++ src/constants/static_variables.cpp
@@ -13,13 +13,9 @@ namespace global_const {
   const int screen_x = 1000;
   const int screen_y = 672;
 
-	const double meter_to_pixel_ratio = 0.5/8.4;
+  const double meter_to_pixel_ratio = 0.5/8.4;
   const double screen_height_meters = double(screen_y) * meter_to_pixel_ratio;
   const double screen_width_meters = double(screen_x) * meter_to_pixel_ratio;
-
-  const double width_angle_of_view = 120; // degrees
-  const double height_angle_of_view = 120; // degrees
-  const double zoom_scalar = 1.0;
 
   const double hop_angle = 1.0;
   const double movement_speed = 5.0; 
